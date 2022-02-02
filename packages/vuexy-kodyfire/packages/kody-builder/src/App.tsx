03071bad4f9e87708e0c9be383c9d257f6245394
Anis Marrouchi <anismarrouchi@hotmail.com>
2022-02-02 17:53:55 +0100

    feat: add a web server for creating the schema file

---
+++ packages/vuexy-kodyfire/packages/kody-builder/src/App.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import './App.css';
+import { person } from '@jsonforms/examples';
+
+import {
+  materialRenderers,
+  materialCells,
+} from '@jsonforms/material-renderers';
+
+import { JsonForms } from '@jsonforms/react';
+function App() {
+  const schema = person.schema;
+  const uischema = person.uischema;
+  const initialData = person.data;
+  const [data, setData] = useState(initialData);
+  return (
+    <div className="App">
+      <JsonForms
+        schema={schema}
+        uischema={uischema}
+        data={data}
+        renderers={materialRenderers}
+        cells={materialCells}
+        onChange={({ data, errors }) => setData(data)}
+      />
+    </div>
+  );
+}
+
+export default App;
