64cca97a243a289ecd22ff5bb376728787d37c5d
Anis Marrouchi <anismarrouchi@hotmail.com>
2022-02-03 08:17:00 +0100

    refactor: move blank-kodyfire into packages

---
+++ packages/kodyfire-kodyfire/packages/vuexy-kodyfire/packages/blank-kodyfire/src/concepts/kodyfire.ts
@@ -0,0 +1,39 @@
+import { apply, applyTemplates, chain,  mergeWith, move, url } from "@angular-devkit/schematics";
+import { IConcept, ITechnology, Source, TemplateSchema } from "kodyfire-core";
+import { join, relative } from "path";
+
+import { strings } from "@angular-devkit/core";
+
+export class Kodyfire implements IConcept {
+    source?: Source;
+  name: string;
+  content: string;
+  outputDir: string;
+  template: TemplateSchema;
+  defaultAction: string;
+  technology: ITechnology;
+  constructor(concept: Partial<IConcept>, technology: ITechnology) {
+    this.source = concept.source ?? Source.Template;
+    this.outputDir = concept.outputDir ?? "";
+    this.name = concept.name ?? "";
+    this.template = concept.template as TemplateSchema;
+    this.technology = technology;
+  }
+    generate(_data: any) {
+        // clone == apply, create == create, overwrite == overwrite
+        const templateSource = apply(
+            url(join(process.cwd(),
+            relative(process.cwd(), __dirname),
+            this.template.path
+          )),
+         [
+            applyTemplates({
+              ...strings,
+              ..._data
+            }),
+            move(join(this.technology.rootDir, this.outputDir)),
+          ]);
+          return chain([mergeWith(templateSource)]);
+    }
+
+}
---
+++ packages/kodyfire-kodyfire/packages/vuexy-kodyfire/packages/blank-kodyfire/src/concepts/templates/blank/src/parser/parser.ts
@@ -0,0 +1,51 @@
+import { extract } from "./extractor";
+import { transform } from "./transformer";
+import { load } from "./loader";
+import { IParser, IValidator } from "kodyfire-core";
+var fs = require('fs');
+
+export class Parser implements IParser {
+validator: IValidator;
+requiresExtract: boolean;
+    requiresTansform: boolean;
+    requiresLoad: boolean;
+data: any
+constructor(validator: IValidator) {
+    this.validator = validator;
+    this.requiresExtract = false;
+    this.requiresTansform = false;
+    this.requiresLoad = false;
+}
+reader(source: string) {
+    return this.readfile(source);
+}
+
+parse(data: any) {
+    if(!this.validate(data)) {
+        return false;  
+    }
+    this.data = data;
+    return data;
+}
+    
+validate = (data: any) => {
+    return this.validator.validate(data);
+}
+extractor:any = () => {
+    // @todo: v.x for source code, reverse engineer websites or other sources that requires an extractor
+    return extract();
+}
+
+transformer = () => {
+    // @todo: v.x for source code, reverse engineer websites or other sources that requires an extractor
+    return transform();
+}
+
+loader = () => {
+    return load();
+}
+readfile(filepath: any) {
+        const fileContent = fs.readFileSync(filepath);
+        return JSON.parse(fileContent);
+  }
+}
