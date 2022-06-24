42373571c42f5c82eceb99bbc47c6a032ef040cb
Anis Marrouchi <anismarrouchi@hotmail.com>
2022-06-24 18:13:39 +0100

    feat(laravel): update instead of overwriting generated artifact

--- packages/laravel-kodyfire/src/concepts/engine.ts
+++ packages/laravel-kodyfire/src/concepts/engine.ts
@@ -3,6 +3,11 @@ import * as builder from 'handlebars';
 import { join, relative, dirname } from 'path';
 const fs = require('fs');
 const fsPromises = fs.promises;
+// Load the writer
+// const writer = require('php-writer');
+const parser = require('php-parser');
+const unparse = require('php-unparser');
+
 export class Engine {
   builder: any;
   constructor() {
@@ -94,12 +99,130 @@ export class Engine {
     rootDir: string,
     outputDir: string,
     filename: any,
-    content: string | Buffer
+    content: string | Buffer,
+    overwrite = false
   ) {
     filename = join(rootDir, outputDir, filename);
+    if (!overwrite) {
+      content = this.setContent(filename, content);
+    }
     // We need to create the directory if it doesn't exist
     await fsPromises.mkdir(dirname(filename), { recursive: true });
     await fsPromises.writeFile(filename, content);
+  }
+  setContent(filename: any, content: string | Buffer): string | Buffer {
+    try {
+      if (fs.existsSync(filename)) {
+        const oldContent = fs.readFileSync(filename);
+
+        const oldMethods = this.getAstMethods(oldContent);
+        const newMethods = this.getAstMethods(content);
+        const options = {
+          indent: true,
+          dontUseWhitespaces: false,
+          shortArray: true,
+          bracketsNewLine: true,
+          forceNamespaceBrackets: false,
+          collapseEmptyLines: true,
+        };
+
+        const ast = this.getAst(content);
+        const updatedMethods = [];
+        for (let i = 0; i < newMethods.length; i++) {
+          const newMethod = newMethods[i];
+          if (!newMethod.name) {
+            continue;
+          }
+          const oldMethod = oldMethods.find((m: any) => {
+            if (m.name === newMethod.name) {
+              return true;
+            }
+          });
+          if (oldMethod) {
+            updatedMethods.push(oldMethod);
+          }
+        }
+
+        for (let i = 0; i < oldMethods.length; i++) {
+          const oldMethod = oldMethods[i];
+          if (!oldMethod.name) {
+            continue;
+          }
+          const foundMethod = newMethods.find((m: any) => {
+            if (m.name && m.name === oldMethod.name) {
+              return true;
+            }
+          });
+          if (!foundMethod) {
+            updatedMethods.push(oldMethod);
+          }
+        }
+
+        const astClassIndex = ast.children[0].children?.findIndex(
+          (c: { kind: string }) => c.kind === 'class'
+        );
+        if (astClassIndex) {
+          ast.children[0].children[astClassIndex].body = updatedMethods;
+        }
+
+        return unparse(ast, options);
+      }
+    } catch (error) {
+      // contine silently
+      // @todo: elaborate error handling
+      console.log(filename, error.message);
+    }
+    return content;
+  }
+
+  private getAstMethods(content: any) {
+    const ast = this.getAst(content);
+    const _class = this.getAstClass(ast);
+    if (_class) {
+      return (
+        _class.body.filter((c: { kind: string }) => c.kind === 'method') || []
+      );
+    }
+    return [];
+  }
+
+  private getAstClass(ast: any) {
+    return ast.children[0].children?.find(
+      (c: { kind: string }) => c.kind === 'class'
+    );
+  }
+
+  private getAst(content: any) {
+    const phpParser = this.getPhpParser();
+    const ast = phpParser.parseCode(content);
+    return ast;
+  }
+
+  private getPhpParser() {
+    return new parser({
+      writer: {
+        indent: true,
+        dontUseWhitespaces: false,
+        shortArray: true,
+        forceNamespaceBrackets: false,
+      },
+      parser: {
+        debug: false,
+        locations: false,
+        extractDoc: false,
+        suppressErrors: false,
+      },
+      lexer: {
+        all_tokens: false,
+        comment_tokens: false,
+        mode_eval: false,
+        asp_tags: false,
+        short_tags: false,
+      },
+      ast: {
+        withPositions: true,
+      },
+    });
   }
 
   async getFiles(rootDir: string, outputDir: string) {
