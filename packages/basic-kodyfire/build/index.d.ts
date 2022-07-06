e52b7a0186f25bc0372f98290091b4dbbdc70a6e
Anis Marrouchi <anismarrouchi@hotmail.com>
2022-07-06 18:40:25 +0100

    fix(basic): export Engine

--- packages/basic-kodyfire/build/index.d.ts
+++ packages/basic-kodyfire/build/index.d.ts
@@ -2,5 +2,6 @@ export { Kody } from './kody';
 export * from './parser';
 export * from './generator';
 export { Technology } from './technology';
+export { Engine } from './concepts/engine';
 export { Concept } from './concepts/concept';
 export { schema } from './parser/validator/schema';
--- packages/basic-kodyfire/build/index.js
+++ packages/basic-kodyfire/build/index.js
@@ -10,13 +10,15 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
     for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.schema = exports.Concept = exports.Technology = exports.Kody = void 0;
+exports.schema = exports.Concept = exports.Engine = exports.Technology = exports.Kody = void 0;
 var kody_1 = require("./kody");
 Object.defineProperty(exports, "Kody", { enumerable: true, get: function () { return kody_1.Kody; } });
 __exportStar(require("./parser"), exports);
 __exportStar(require("./generator"), exports);
 var technology_1 = require("./technology");
 Object.defineProperty(exports, "Technology", { enumerable: true, get: function () { return technology_1.Technology; } });
+var engine_1 = require("./concepts/engine");
+Object.defineProperty(exports, "Engine", { enumerable: true, get: function () { return engine_1.Engine; } });
 var concept_1 = require("./concepts/concept");
 Object.defineProperty(exports, "Concept", { enumerable: true, get: function () { return concept_1.Concept; } });
 var schema_1 = require("./parser/validator/schema");
