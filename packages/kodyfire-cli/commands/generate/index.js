1861c864f2a4a3dc504d9730bcad15339a982c85
Anis Marrouchi <anismarrouchi@hotmail.com>
2022-08-01 17:47:55 +0100

    feat(cli): pass concept as default for generatecmd

--- packages/kodyfire-cli/commands/generate/index.js
+++ packages/kodyfire-cli/commands/generate/index.js
@@ -41,7 +41,7 @@ module.exports = program => {
     .alias('g')
     .description('Prompt assistant to quickly generate an artifact')
     .argument('[kody]', 'The name of the kody to use')
-    .argument('[concept]', 'The concept you want to generate')
+    .argument('[concept]', 'The concept you want to generate', 'concept')
     .option('-m,--multiple', 'Generate multiple artifacts')
     .option('-p,--persist', 'Persist the generated artifact')
     .action((kody, concept, _opt) =>
