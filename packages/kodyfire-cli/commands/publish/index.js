9eb68cfc4cc68e42450190ad76566f8aa1de7ee1
Anis Marrouchi <anismarrouchi@hotmail.com>
2023-03-11 13:44:52 +0100

    feat(cli): allow publishing individual template

--- packages/kodyfire-cli/commands/publish/index.js
+++ packages/kodyfire-cli/commands/publish/index.js
@@ -9,6 +9,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+// @ts-nocheck
 const fs_1 = require("fs");
 const kodyfire_core_1 = require("kodyfire-core");
 const chalk = require('chalk');
@@ -17,6 +18,7 @@ const { Command } = require('commander');
 const { promises: fs } = require('fs');
 const path = require('path');
 const action = (args) => __awaiter(void 0, void 0, void 0, function* () {
+    const { template } = args;
     const kodies = yield kodyfire_core_1.Package.getInstalledKodies();
     if (kodies.length == 0) {
         const kody = chalk.greenBright(chalk.bold('kody'));
@@ -34,16 +36,38 @@ const action = (args) => __awaiter(void 0, void 0, void 0, function* () {
             ? `${args.name}-kodyfire`
             : args.name;
         if (kodies.find(kody => kody.name == name)) {
+            // @todo: allow specifing target template path
             const target = `./.kody/${name}/templates`;
+            // @todo: allow specifing source template paths
             const sources = [
                 `./node_modules/${name}/src/concepts/templates`,
                 `./node_modules/${name}/src/templates`,
             ];
+            let templateFound = false;
             //for each source folder if it exists copy it to the target folder
             for (const source of sources) {
-                if ((0, fs_1.existsSync)(source)) {
-                    yield copyDir(source, target);
+                if (template) {
+                    if ((0, fs_1.existsSync)(`${source}/${template}`)) {
+                        yield copyFileOrDir(`${source}/${template}`, `${target}`, `${template}`);
+                        templateFound = true;
+                        continue;
+                    }
                 }
+                else {
+                    if ((0, fs_1.existsSync)(source)) {
+                        yield copyDir(source, target);
+                    }
+                }
+            }
+            if (template && !templateFound) {
+                console.log(boxen(`😞 Template ${chalk.yellow(chalk.bold(template))} not found`, {
+                    padding: 1,
+                    margin: 1,
+                    align: 'center',
+                    borderColor: 'yellow',
+                    borderStyle: 'round',
+                }));
+                process.exit(1);
             }
             //copy the assets.(json|js) file
             if ((0, fs_1.existsSync)(`./node_modules/${name}/src/assets.js`)) {
@@ -62,6 +86,19 @@ const action = (args) => __awaiter(void 0, void 0, void 0, function* () {
         }
     }
 });
+function copyFileOrDir(src, dest, template) {
+    return __awaiter(this, void 0, void 0, function* () {
+        yield fs.mkdir(dest, { recursive: true });
+        const destPath = path.join(dest, template);
+        const stats = yield fs.lstat(src);
+        if (stats.isDirectory()) {
+            yield copyDir(src, destPath);
+        }
+        else {
+            yield fs.copyFile(src, destPath);
+        }
+    });
+}
 function copyDir(src, dest) {
     return __awaiter(this, void 0, void 0, function* () {
         yield fs.mkdir(dest, { recursive: true });
@@ -79,9 +116,10 @@ module.exports = (program) => {
     program
         .command('publish')
         .argument('<kody>', 'kody name to publish')
+        .argument('[template]', 'The abreviated or full template name', false)
         .description('Publish the templates of the kody along with the assets.json and schema.ts files')
-        .action((name, _opt) => __awaiter(void 0, void 0, void 0, function* () {
-        action(Object.assign({ name }, _opt));
+        .action((name, template, _opt) => __awaiter(void 0, void 0, void 0, function* () {
+        action(Object.assign({ name, template }, _opt));
     }));
 };
 //# sourceMappingURL=index.js.map
--- packages/kodyfire-cli/commands/publish/index.ts
+++ packages/kodyfire-cli/commands/publish/index.ts
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import { existsSync } from 'fs';
 import { Package } from 'kodyfire-core';
 const chalk = require('chalk');
@@ -7,6 +8,7 @@ const { promises: fs } = require('fs');
 const path = require('path');
 
 const action = async (args: any) => {
+  const { template } = args;
   const kodies = await Package.getInstalledKodies();
 
   if (kodies.length == 0) {
@@ -26,16 +28,39 @@ const action = async (args: any) => {
       ? `${args.name}-kodyfire`
       : args.name;
     if (kodies.find(kody => kody.name == name)) {
+      // @todo: allow specifing target template path
       const target = `./.kody/${name}/templates`;
+      // @todo: allow specifing source template paths
       const sources = [
         `./node_modules/${name}/src/concepts/templates`,
         `./node_modules/${name}/src/templates`,
       ];
+      let templateFound = false;
       //for each source folder if it exists copy it to the target folder
       for (const source of sources) {
+        if (template) {
+          if (existsSync(`${source}/${template}`)) {
+          await copyFileOrDir(`${source}/${template}`, `${target}`, `${template}`);
+          templateFound = true;
+          continue;
+          }
+        } else {
         if (existsSync(source)) {
-          await copyDir(source, target);
+            await copyDir(source, target);
+          }
         }
+      }
+      if (template && !templateFound) {
+        console.log(
+          boxen(`😞 Template ${ chalk.yellow(chalk.bold(template))} not found`, {
+            padding: 1,
+            margin: 1,
+            align: 'center',
+            borderColor: 'yellow',
+            borderStyle: 'round',
+          })
+        );
+        process.exit(1);
       }
       //copy the assets.(json|js) file
       if (existsSync(`./node_modules/${name}/src/assets.js`)) {
@@ -63,10 +88,19 @@ const action = async (args: any) => {
   }
 };
 
+async function copyFileOrDir(src: any, dest: any, template: string) {
+  await fs.mkdir(dest, { recursive: true });
+  const destPath = path.join(dest, template);
+  const stats = await fs.lstat(src);
+  if (stats.isDirectory()) {
+    await copyDir(src, destPath);
+  } else {
+    await fs.copyFile(src, destPath);
+  }
+}
 async function copyDir(src: any, dest: any) {
   await fs.mkdir(dest, { recursive: true });
   const entries = await fs.readdir(src, { withFileTypes: true });
-
   for (const entry of entries) {
     const srcPath = path.join(src, entry.name);
     const destPath = path.join(dest, entry.name);
@@ -81,10 +115,11 @@ module.exports = (program: typeof Command) => {
   program
     .command('publish')
     .argument('<kody>', 'kody name to publish')
+    .argument('[template]', 'The abreviated or full template name', false)
     .description(
       'Publish the templates of the kody along with the assets.json and schema.ts files'
     )
-    .action(async (name: string, _opt: any) => {
-      action({ name, ..._opt });
+    .action(async (name: string, template, _opt: any) => {
+      action({ name, template, ..._opt });
     });
 };
